483b4f09d7669bfb8ecf5905d40c51212b990ff7
HuseyinKoroglu <korogluh27@gmail.com>
2025-08-08 11:29:37 +0300

    router model added

---
+++ models/routeModel.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column , String , Integer
+from db.database import Base
+
+class Route(Base):
+    __tablename__ = "routes"
+    route_id = Column(String,primary_key=True)
+    agency_id = Column(String)
+    route_short_name = Column(String)
+    route_long_name = Column(String)
+    route_desc = Column(String)
+    route_type = Column(String)
+    route_url = Column(String)
+    route_color = Column(String)
+    route_text_color = Column(String)
