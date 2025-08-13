d89d794926e08c7150c5a2eee6fb051c2cfbcd77
HuseyinKoroglu <korogluh27@gmail.com>
2025-08-13 17:02:20 +0300

    shape model is written

---
+++ models/shapesModel.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, String, Integer, Float, PrimaryKeyConstraint
+from db.database import Base
+
+class Shape(Base):
+    __tablename__ = "shapes"
+    
+    shape_id = Column(String, primary_key=True)
+    shape_pt_sequence = Column(Integer, primary_key=True)
+    
+    shape_pt_lat = Column(Float)  
+    shape_pt_lon = Column(Float)  
+    shape_dist_traveled = Column(String)  
+    
+    __table_args__ = (
+        PrimaryKeyConstraint('shape_id', 'shape_pt_sequence'),
+    )
